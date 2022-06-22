4f383d779cafe8a31e652e099f127060a76caf90
Andrew <joroveanuandrei@gmail.com>
2022-06-22 10:07:58 +0300

    Added blank project

---
+++ src/main/java/com/ajpv/magazinmobila/MagazinMobilaApplication.java
@@ -0,0 +1,13 @@
+package com.ajpv.magazinmobila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinMobilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinMobilaApplication.class, args);
+	}
+
+}
